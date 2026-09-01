824d111c4d613bf59eade2da7f09417d5b73eb40
ItsZix <anghe@ejemplo.com>
2026-09-01 15:48:14 -0500

    Lanzamiento inicial PISQA en Castilla

--- organizar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Organizando y limpiando el proyecto...
-powershell.exe -ExecutionPolicy Bypass -File organizar.ps1
-echo =======================================================
-echo ¡Todo listo! 
-echo El proyecto final ahora esta unicamente en la carpeta pisqa-vercel.
-echo Los archivos de afuera han sido limpiados.
-echo =======================================================
-pause
